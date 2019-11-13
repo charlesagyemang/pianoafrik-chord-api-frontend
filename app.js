dbaed08a8aa7784c8c491db0b993038cfd22226b
charlesagyemang <micnkru@gmail.com>
2019-11-13 02:12:33 +0000

    Init

---
+++ app.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+//
+//   //------- URLS -------//
+//   const localUrl        = 'http://localhost:8081';
+//   const loginUrl        = `${localUrl}/v2/user/login`;
+//   const loginSimpleUrl  = `${localUrl}/v2/user/simple.login`;
+//   const registerUrl     = `${localUrl}/v2/register`;
+//
+//
+// //------ Post Bodies ------//
+//   const bodyToSend    = {
+//     name: "charles"
+//   }
+//
+// const loginBody = {
+//     "email" : "para2@gmail.com",
+//     "password" : "password",
+//     "facility": "5dae2dbe6aa4ab7eaf53c35b",
+//     "domain" : "kobiti"
+// }
+//
+//
+// const loginSimpleBody = {
+//     "email" : "para2@gmail.com",
+//     "password" : "password"
+// }
+//
+// const registerBody = {
+// 	"email" : "maloon@gmail.com",
+// 	"password" : "password",
+// 	"facility": "5dae2dbe6aa4ab7eaf53c35b"
+// }
+//
+//
+// // headers
+// const cors = {
+//   'X-Requested-With': 'XMLHttpRequest',
+// }
+//
+// const headerWithoutAuth = { headers: cors }
+//
+//
+// 
+//   axios.post( loginSimpleUrl, loginSimpleBody, headerWithoutAuth)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+})
